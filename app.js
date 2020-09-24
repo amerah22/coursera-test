2ccf5a30324b1537bc40bfbf2bd0bd2d01685ebd
Amerah Al-gaidi <meesha1400@gmail.com>
2020-09-25 00:39:23 +0300

    edit on app js

---
+++ app.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict'
+  
+angular.module('myFirstApp', [])
+// angular object ti execute thing on it like module define our module takes a name
+//for the app and list of dependesies ( we put a an empty array)
+.controller('MyFirstController' , function() {
+
+
+});
+
+
+})();
+//IIFE no local variables bleed into the global scoop
