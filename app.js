2d910cfeb5f2d76858c4bbcca4fe9f510805adbc
Amerah Al-gaidi <meesha1400@gmail.com>
2020-09-26 21:34:03 +0300

    $scope

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 (function() {
   'use strict'
-  
+
 angular.module('myFirstApp', [])
 // angular object ti execute thing on it like module define our module takes a name
 //for the app and list of dependesies ( we put a an empty array)
-.controller('MyFirstController' , function() {
+.controller('MyFirstController' , function($scope) {
 
+$scope.name = "Amerah" ;
 
 });
 
 
 })();
 //IIFE no local variables bleed into the global scoop
+// $scope var to share data betwwen viewmodel (index ) and view
+// $scope is provided pay angularjs
