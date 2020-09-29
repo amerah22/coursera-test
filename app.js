90695bf622ab22d4c58563590e1a89e0eed17cd0
Amerah Al-gaidi <meesha1400@gmail.com>
2020-09-30 02:11:32 +0300

    app

--- app.js
+++ app.js
@@ -4,16 +4,24 @@
 angular.module('myFirstApp', [])
 // angular object ti execute thing on it like module define our module takes a name
 //for the app and list of dependesies ( we put a an empty array)
-.controller('MyFirstController' , function($scope) {
+.controller('MyFirstController' , MyFirstController );
+MyFirstController.$inject = ['$scope'];
 
-$scope.name = "Amerah" ;
+function MyFirstController($scope) {
 
-$scope.SayHello = function() {
+  $scope.name = "Amerah" ;
 
-  return "hello everybody";
-};
+  $scope.SayHello = function() {
 
-});
+    return "hello everybody";
+  };
+
+
+}
+
+
+
+
 
 
 })();
